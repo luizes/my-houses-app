a4adb3b5e85a3f2be90354732ca2074e6fd3c9f7
luizes <luizfernandoschaves@gmail.com>
2017-11-27 13:20:35 -0300

    Arquivo firebase.config.ts com as configuração do banco. Leitura e escrita no banco de dados em tempo real.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -8,6 +10,8 @@ import { MyApp } from './app.component';
 import { WelcomePage } from '../pages/welcome/welcome';
 import { HomePage } from '../pages/home/home';
 
+import { FirebaseConfig } from '../../firebase.config';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -16,7 +20,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FirebaseConfig),
+    AngularFirestoreModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    private _housesCollection: AngularFirestoreCollection<any>;
+    public houses: Observable<any[]>;
 
-  constructor(public navCtrl: NavController) {
+    constructor(private _firestore: AngularFirestore) {
+        this._housesCollection = _firestore.collection('houses');
+        this.houses = this._housesCollection.valueChanges();
+    }
 
-  }
-
+    public add(): void {
+        this._housesCollection.add({ describe: 'testestests' });
+    }
 }
