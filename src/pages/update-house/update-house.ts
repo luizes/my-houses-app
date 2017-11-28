8ebd436df954b8f43090b0bc198db87e827b6ce3
luizes <luizfernandoschaves@gmail.com>
2017-11-28 13:14:09 -0300

    Página para edição de casas.

---
+++ src/pages/update-house/update-house.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'update-house',
+    templateUrl: './update-house.html'
+})
+export class UpdateHousePage implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+    public updateHouse(): void {
+
+    }
+}
